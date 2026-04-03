29e3c2934beae5437d2305122ef10b63384fd360
Harita-Nealife <131236027+Harita-Nealife@users.noreply.github.com>
2026-04-03 11:47:05 +0530

    Create UserDTO class

---
+++ src/main/java/com/example/dtos/UserDTO.java
@@ -0,0 +1,13 @@
+public class UserDTO {
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+    private String userRole;
+    private String regionAccess;
+    private boolean isCompanyWideAccess;
+
+    // Getters and Setters
+    // ...
+}
